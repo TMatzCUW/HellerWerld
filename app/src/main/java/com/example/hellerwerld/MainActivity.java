8dfcc2322edfccb99f041a9f14913a1f2b089afe
TMatzCUW <70924911+TMatzCUW@users.noreply.github.com>
2020-11-19 10:03:50 -0600

    Updated for HW13

--- app/src/main/java/com/example/hellerwerld/MainActivity.java
+++ app/src/main/java/com/example/hellerwerld/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.hellerwerld;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
+import android.widget.TextView;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClicked(View V){
+        TextView textView=this.findViewById(R.id.textView);
+        textView.setText("Billy");
+    }
+
+    public void onButtonClicked2(View V){
+        TextView textView2=this.findViewById(R.id.textView2);
+        textView2.setText("Mays");
     }
 }
